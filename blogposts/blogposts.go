d207ff848db0280f6e2b2e3d998835a3e8d0c228
Jonas Gacrama <jonasgacrama@gmail.com>
2022-07-13 10:55:47 +0800

    blogposts: get and read are now separate functions
    But shouldn't newPost() be renamed to readPost()

--- blogposts/blogposts.go
+++ blogposts/blogposts.go
@@ -39,14 +39,16 @@ func getPost(fileSystem fs.FS, f fs.DirEntry) (Post, error) {
 	if err != nil {
 		return Post{}, err
 	}
+	defer postFile.Close()
+	return newPost(postFile)
+}
 
+func newPost(postFile fs.File) (Post, error) {
 	postData, err := io.ReadAll(postFile)
 	if err != nil {
 		return Post{}, err
 	}
-
 	post := Post{Title: string(postData)[7:]}
-	defer postFile.Close()
 	return post, nil
 }
 
