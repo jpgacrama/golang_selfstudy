2a68b8088689637f80f2a475b0f5195cb0f04f80
Jonas Gacrama <jonasgacrama@gmail.com>
2022-07-13 10:33:16 +0800

    blogposts: Implemented getPost() to get posts from the File system

--- blogposts/blogposts.go
+++ blogposts/blogposts.go
@@ -2,6 +2,7 @@ package blogposts
 
 import (
 	"errors"
+	"io"
 	"io/fs"
 	"testing/fstest"
 )
@@ -20,10 +21,33 @@ func NewPostsFromFS(fileSystem fstest.MapFS) ([]Post, error) {
 	}
 	var posts []Post
 
-	for range dir {
-		posts = append(posts, Post{})
+	for _, f := range dir {
+		post, err := getPost(fileSystem, f)
+		if err != nil {
+			//todo: needs clarification,
+			// should we totally fail if one file fails? or just ignore?
+			return nil, err
+		}
+		posts = append(posts, post)
 	}
+
 	return posts, nil
+}
+
+func getPost(fileSystem fs.FS, f fs.DirEntry) (Post, error) {
+	postFile, err := fileSystem.Open(f.Name())
+	if err != nil {
+		return Post{}, err
+	}
+
+	postData, err := io.ReadAll(postFile)
+	if err != nil {
+		return Post{}, err
+	}
+
+	post := Post{Title: string(postData)[7:]}
+	defer postFile.Close()
+	return post, nil
 }
 
 func (s StubFailingFS) Open(name string) (fs.File, error) {
