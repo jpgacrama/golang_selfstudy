1cd1e4d1bf168928835c83a7b1bc1501bd45e8c7
Jonas Gacrama <jonasgacrama@gmail.com>
2022-07-16 10:34:43 +0800

    blogrenderer: Started creating the test code

---
+++ blogrenderer/blogrenderer_test/blogrenderer_test.go
@@ -0,0 +1,33 @@
+package blogrenderer_test
+
+import (
+	"bytes"
+	"github.com/jpgacrama/golang_selfstudy/blogrenderer"
+	"testing"
+)
+
+func TestRender(t *testing.T) {
+	var (
+		aPost = blogrenderer.Post{
+			Title:       "hello world",
+			Body:        "This is a post",
+			Description: "This is a description",
+			Tags:        []string{"go", "tdd"},
+		}
+	)
+
+	t.Run("Converts a single post into HTML", func(t *testing.T) {
+		buf := bytes.Buffer{}
+		err := blogrenderer.Render(&buf, aPost)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		got := buf.String()
+		want := `<h1>hello world</h1>`
+		if got != want {
+			t.Errorf("got '%s' want '%s'", got, want)
+		}
+	})
+}
