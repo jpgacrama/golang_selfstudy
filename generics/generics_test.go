4c5a6589554fa8ac9f2b3cf67749a94b32e4cc3d
Jonas Gacrama <jonasgacrama@gmail.com>
2022-07-18 14:53:47 +0800

    master: Removed extra test inside generics

--- generics/generics_test.go
+++ generics/generics_test.go
@@ -4,18 +4,6 @@ import (
 	"testing"
 )
 
-func TestAssertFunctions(t *testing.T) {
-	t.Run("asserting on integers", func(t *testing.T) {
-		AssertEqual(t, 1, 1)
-		AssertNotEqual(t, 1, 2)
-	})
-
-	t.Run("asserting on strings", func(t *testing.T) {
-		AssertEqual(t, "hello", "hello")
-		AssertNotEqual(t, "hello", "Grace")
-	})
-
-}
 func TestStack(t *testing.T) {
 	t.Run("integer stack", func(t *testing.T) {
 		myStackOfInts := new(Stack[int])
