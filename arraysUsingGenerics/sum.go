2aea42d2032ef4d8240fcf4606c92a3a23e40c25
Jonas Gacrama <jonasgacrama@gmail.com>
2022-07-20 12:06:29 +0800

    generics: Using the Reduce technique allows us to extract business logic
    That's because all the implementation details are handled by the generic Reduce() function

--- arraysUsingGenerics/sum.go
+++ arraysUsingGenerics/sum.go
@@ -1,24 +1,5 @@
 package arraysusinggenerics
 
-type Transaction struct {
-	From string
-	To   string
-	Sum  float64
-}
-
-func BalanceFor(transactions []Transaction, name string) float64 {
-	adjustBalance := func(currentBalance float64, t Transaction) float64 {
-		if t.From == name {
-			return currentBalance - t.Sum
-		}
-		if t.To == name {
-			return currentBalance + t.Sum
-		}
-		return currentBalance
-	}
-	return Reduce(transactions, adjustBalance, 0.0)
-}
-
 func Sum(numbers []int) int {
 	add := func(acc, x int) int { return acc + x }
 	return Reduce(numbers, add, 0)
