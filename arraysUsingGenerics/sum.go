54e372c364d2b3c2befff01184bffa5c787d23b8
Jonas Gacrama <jonasgacrama@gmail.com>
2022-07-20 09:05:30 +0800

    generics: Created a Reduce() function

--- arraysUsingGenerics/sum.go
+++ arraysUsingGenerics/sum.go
@@ -1,9 +1,17 @@
-package arraysusinggenerics
+package arrays
 
-// Sum calculates the total from a slice of numbers.
 func Sum(numbers []int) int {
 	add := func(acc, x int) int { return acc + x }
 	return Reduce(numbers, add, 0)
+}
+
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return sums
 }
 
 // SumAllTails calculates the sums of all but the first number given a collection of slices.
@@ -18,4 +26,12 @@ func SumAllTails(numbers ...[]int) []int {
 	}
 
 	return Reduce(numbers, sumTail, []int{})
+}
+
+func Reduce[A any](collection []A, accumulator func(A, A) A, initialValue A) A {
+	var result = initialValue
+	for _, x := range collection {
+		result = accumulator(result, x)
+	}
+	return result
 }
