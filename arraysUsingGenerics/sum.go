48b8b6fc01c4cf79fbfe08fd9c6a5c64ffca3913
Jonas Gacrama <jonasgacrama@gmail.com>
2022-07-20 12:00:22 +0800

    generics: Created bank.go and reduce.go files

--- arraysUsingGenerics/sum.go
+++ arraysUsingGenerics/sum.go
@@ -46,11 +46,3 @@ func SumAllTails(numbers ...[]int) []int {
 
 	return Reduce(numbers, sumTail, []int{})
 }
-
-func Reduce[A, B any](collection []A, accumulator func(B, A) B, initialValue B) B {
-	var result = initialValue
-	for _, x := range collection {
-		result = accumulator(result, x)
-	}
-	return result
-}
