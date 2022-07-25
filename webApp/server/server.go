f38e71992aec9475cf85b965a20f6aa6ab9e5b00
Jonas Gacrama <jonasgacrama@gmail.com>
2022-07-25 13:12:39 +0800

    webApp: Removed redundant code to trim prefix
    This functionality is now placed inside ServeHTTP()

--- webApp/server/server.go
+++ webApp/server/server.go
@@ -16,27 +16,27 @@ type PlayerServer struct {
 }
 
 func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case http.MethodPost:
-		p.processWin(w, r)
-	case http.MethodGet:
-		p.showScore(w, r)
-	}
-
-}
-
-func (p *PlayerServer) showScore(w http.ResponseWriter, r *http.Request) {
 	player := strings.TrimPrefix(r.URL.Path, "/players/")
 
+	switch r.Method {
+	case http.MethodPost:
+		p.processWin(w, player)
+	case http.MethodGet:
+		p.showScore(w, player)
+	}
+}
+
+func (p *PlayerServer) showScore(w http.ResponseWriter, player string) {
 	score := p.Store.GetPlayerScore(player)
+
 	if score == 0 {
 		w.WriteHeader(http.StatusNotFound)
 	}
+
 	fmt.Fprint(w, score)
 }
 
-func (p *PlayerServer) processWin(w http.ResponseWriter, r *http.Request) {
-	player := strings.TrimPrefix(r.URL.Path, "/players/")
+func (p *PlayerServer) processWin(w http.ResponseWriter, player string) {
 	p.Store.RecordWin(player)
 	w.WriteHeader(http.StatusAccepted)
 }
