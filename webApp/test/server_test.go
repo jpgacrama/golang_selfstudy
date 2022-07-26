68ebd394e69af7db61f36e16af4e2cbdf90cc685
Jonas Gacrama <jonasgacrama@gmail.com>
2022-07-26 09:30:45 +0800

    webApp: ServeHTTP() now supports /league
    But this will turn into a monolith if we are not careful

--- webApp/test/server_test.go
+++ webApp/test/server_test.go
@@ -77,6 +77,19 @@ func TestStoreWins(t *testing.T) {
 	})
 }
 
+func TestLeague(t *testing.T) {
+	store := StubPlayerStore{}
+	server := &server.PlayerServer{Store: &store}
+
+	t.Run("it returns 200 on /league", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "/league", nil)
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+		assertStatus(t, response.Code, http.StatusOK)
+	})
+}
+
 func assertStatus(t testing.TB, got, want int) {
 	t.Helper()
 	if got != want {
