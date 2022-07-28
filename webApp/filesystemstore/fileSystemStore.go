7c4300e3bd0b8a400bd44cdbd3911f2e3d001a89
Jonas Gacrama <jonasgacrama@gmail.com>
2022-07-28 15:21:21 +0800

    webApp: NewFileSystemPlayerStore() now has error checking
    But it is getting to big.. Maybe it's time to refactor this?

--- webApp/filesystemstore/fileSystemStore.go
+++ webApp/filesystemstore/fileSystemStore.go
@@ -29,6 +29,14 @@ func (t *Tape) SetFile(f *os.File) {
 
 func NewFileSystemPlayerStore(file *os.File) (*FileSystemPlayerStore, error) {
 	file.Seek(0, 0)
+	info, err := file.Stat()
+	if err != nil {
+		return nil, fmt.Errorf("problem getting file info from file %s, %v", file.Name(), err)
+	}
+	if info.Size() == 0 {
+		file.Write([]byte("[]"))
+		file.Seek(0, 0)
+	}
 	league, err := league.NewLeague(file)
 	if err != nil {
 		return nil, fmt.Errorf("problem loading player store from file %s, %v", file.Name(), err)
--- webApp/test/file_system_store_test.go
+++ webApp/test/file_system_store_test.go
@@ -83,6 +83,12 @@ func TestFileSystemStore(t *testing.T) {
 		want := 1
 		assertScoreEquals(t, got, want)
 	})
+	t.Run("works with an empty file", func(t *testing.T) {
+		database, cleanDatabase := createTempFile(t, "")
+		defer cleanDatabase()
+		_, err := filesystemstore.NewFileSystemPlayerStore(database)
+		assertNoError(t, err)
+	})
 }
 
 func assertScoreEquals(t testing.TB, got, want int) {
