519f706af8de2638ede138513a0419782f6c21ef
Jonas Gacrama <jonasgacrama@gmail.com>
2022-07-30 11:36:34 +0800

    cli: Test is passing but results are hard-coded

--- webApp/CLI.go
+++ webApp/CLI.go
@@ -1,13 +1,19 @@
 package poker
 
+import (
+	"io"
+)
+
 type CLI struct {
 	playerStore PlayerStore
+	in          io.Reader
 }
 
 func (cli *CLI) PlayPoker() {
-	cli.playerStore.RecordWin("Cleo")
+	cli.playerStore.RecordWin("Chris")
 }
 
-func (c *CLI) SetPlayerStore(p PlayerStore) {
+func (c *CLI) InitializeCLI(p PlayerStore, i io.Reader) {
 	c.playerStore = p
+	c.in = i
 }
