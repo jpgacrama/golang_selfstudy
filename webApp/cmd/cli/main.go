9c807e2546191cf04e93f1127150250d4adb0034
Jonas Gacrama <jonasgacrama@gmail.com>
2022-07-30 18:00:11 +0800

    cli: main.go from both WebServer and CLI use a common FileSystemPlayerStoreFromFile()
    They both do the same thing anyway, so it's something that we can refactor

--- webApp/cmd/cli/main.go
+++ webApp/cmd/cli/main.go
@@ -10,27 +10,13 @@ import (
 const dbFileName = "game.db.json"
 
 func main() {
+	store, close, err := poker.FileSystemPlayerStoreFromFile(dbFileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer close()
+
 	fmt.Println("Let's play poker")
 	fmt.Println("Type {Name} wins to record a win")
-
-	db, err := os.OpenFile(dbFileName, os.O_RDWR|os.O_CREATE, 0666)
-	assertOpeningFileSuccessful(dbFileName, err)
-
-	store, err := poker.NewFileSystemPlayerStore(db)
-	assertCreatingFileSystemPlayerStoreSuccessful(err)
-
-	game := poker.NewCLI(store, os.Stdin)
-	game.PlayPoker()
-}
-
-func assertOpeningFileSuccessful(fileName string, err error) {
-	if err != nil {
-		log.Fatalf("problem opening %s %v", fileName, err)
-	}
-}
-
-func assertCreatingFileSystemPlayerStoreSuccessful(err error) {
-	if err != nil {
-		log.Fatalf("problem creating file system player store, %v ", err)
-	}
+	poker.NewCLI(store, os.Stdin).PlayPoker()
 }
--- webApp/cmd/webserver/main/main.go
+++ webApp/cmd/webserver/main/main.go
@@ -4,21 +4,18 @@ import (
 	"golang_selfstudy/webApp"
 	"log"
 	"net/http"
-	"os"
 )
 
 const dbFileName = "game.db.json"
 
 func main() {
-	db, err := os.OpenFile(dbFileName, os.O_RDWR|os.O_CREATE, 0666)
-	if err != nil {
-		log.Fatalf("problem opening %s %v", dbFileName, err)
-	}
+	store, close, err := poker.FileSystemPlayerStoreFromFile(dbFileName)
 
-	store, err := poker.NewFileSystemPlayerStore(db)
 	if err != nil {
-		log.Fatalf("problem creating file system player store, %v ", err)
+		log.Fatal(err)
 	}
+	defer close()
+
 	server := poker.NewPlayerServer(store)
 
 	if err := http.ListenAndServe(":8080", server); err != nil {
