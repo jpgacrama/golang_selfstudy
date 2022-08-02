e515f95699c696a0bae5fc66c6a596f0d0010785
Jonas Gacrama <jonasgacrama@gmail.com>
2022-08-02 20:34:48 +0800

    time: Fixed compilation errors in BlindAlerterFunc
    But the functions are only returning nil which is not correct 🤔

--- webApp/cmd/cli/main.go
+++ webApp/cmd/cli/main.go
@@ -1,16 +1,24 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"golang_selfstudy/webApp"
+	"log"
 	"os"
 )
 
+const dbFileName = "game.db.json"
+
 func main() {
+	store, close, err := poker.FileSystemPlayerStoreFromFile(dbFileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer close()
+
 	fmt.Println("Let's play poker")
 	fmt.Println("Type {Name} wins to record a win")
-	stdout := &bytes.Buffer{}
-	game := &poker.Game{}
-	poker.NewCLI(os.Stdin, stdout, game)
+	game := poker.NewGame(poker.BlindAlerterFunc(poker.StdOutAlerter), store)
+	cli := poker.NewCLI(os.Stdin, os.Stdout, game)
+	cli.PlayPoker()
 }
