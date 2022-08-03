f685acc5bcf15ad11a07c606cfe6fa78e0667e4a
Jonas Gacrama <jonasgacrama@gmail.com>
2022-08-03 15:24:42 +0800

    time: Added a new test: user enters number of players and starts game

--- webApp/test/cli_test.go
+++ webApp/test/cli_test.go
@@ -104,6 +104,24 @@ func TestCLI(t *testing.T) {
 			})
 		}
 	})
+	t.Run("it prompts the user to enter the number of players and starts the game", func(t *testing.T) {
+		stdout := &bytes.Buffer{}
+		in := strings.NewReader("7\n")
+		game := &poker.GameSpy{}
+
+		cli := poker.NewCLI(in, stdout, game)
+		cli.PlayPoker()
+
+		gotPrompt := stdout.String()
+		wantPrompt := poker.PlayerPrompt
+
+		if gotPrompt != wantPrompt {
+			t.Errorf("got %q, want %q", gotPrompt, wantPrompt)
+		}
+		if game.StartedWith != 7 {
+			t.Errorf("wanted Start called with 7 but got %d", game.StartedWith)
+		}
+	})
 }
 
 func TestGame_Start(t *testing.T) {
